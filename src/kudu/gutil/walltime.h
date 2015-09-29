7392e47bfcaac103d360583d03e9beb06cc3e459
Dan Burkert <dan@cloudera.com>
2015-09-29 13:58:58 -0400

    OS X: compile gutil
    This is a rollup of commits authored by Adar, JD, and me. These changes allow
    the gutil module to be compiled on OS X.
    
    Change-Id: I5c8d80d8d6127efd8f6d94f478e1f1126a3c5338

--- src/kudu/gutil/walltime.h
+++ src/kudu/gutil/walltime.h
@@ -19,6 +19,11 @@
 #include <string>
 using std::string;
 
+#if defined(__APPLE__)
+#include <mach/clock.h>
+#include <mach/mach.h>
+#endif  // defined(__APPLE__)
+
 #include "kudu/gutil/integral_types.h"
 
 typedef double WallTime;
@@ -49,27 +54,52 @@ WallTime WallTime_Now();
 typedef int64 MicrosecondsInt64;
 
 namespace walltime_internal {
+
+#if defined(linux)
 inline MicrosecondsInt64 GetClockTimeMicros(clockid_t clock) {
   timespec ts;
   clock_gettime(clock, &ts);
   return ts.tv_sec * 1000000 + ts.tv_nsec / 1000;
 }
+#elif defined(__APPLE__)
+inline MicrosecondsInt64 GetClockTimeMicros() {
+  clock_serv_t cclock;
+  mach_timespec_t ts;
+  host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
+  clock_get_time(cclock, &ts);
+  mach_port_deallocate(mach_task_self(), cclock);
+  return ts.tv_sec * 1000000 + ts.tv_nsec / 1000;
+}
+#endif  // defined(linux)
+
 } // namespace walltime_internal
 
 // Returns the time since the Epoch measured in microseconds.
 inline MicrosecondsInt64 GetCurrentTimeMicros() {
+#if defined(linux)
   return walltime_internal::GetClockTimeMicros(CLOCK_REALTIME);
+#elif defined(__APPLE__)
+  return walltime_internal::GetClockTimeMicros();
+#endif  // defined(linux)
 }
 
 // Returns the time since some arbitrary reference point, measured in microseconds.
 // Guaranteed to be monotonic (and therefore useful for measuring intervals)
 inline MicrosecondsInt64 GetMonoTimeMicros() {
+#if defined(linux)
   return walltime_internal::GetClockTimeMicros(CLOCK_MONOTONIC);
+#elif defined(__APPLE__)
+  return walltime_internal::GetClockTimeMicros();
+#endif  // defined(linux)
 }
 
 // Returns the time spent in user CPU on the current thread, measured in microseconds.
 inline MicrosecondsInt64 GetThreadCpuTimeMicros() {
+#if defined(linux)
   return walltime_internal::GetClockTimeMicros(CLOCK_THREAD_CPUTIME_ID);
+#elif defined(__APPLE__)
+  return walltime_internal::GetClockTimeMicros();
+#endif  // defined(linux)
 }
 
 // A CycleClock yields the value of a cycle counter that increments at a rate
